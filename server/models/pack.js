5de34b30502ebd7ae6622eded9b0fca4de602e84
Servellon <servellonrebeca99@gmail.com>
2023-09-16 17:25:35 -0600

    update pack model

--- server/models/pack.js
+++ server/models/pack.js
@@ -3,7 +3,11 @@ import mongoose from 'mongoose';
 const packSchema = mongoose.Schema({
     name: String,
     available: Boolean,
-    price: Number
+    price: Number,
+    image: {
+        data: Buffer, // Almacenar los datos de la imagen como un buffer
+        contentType: String // Tipo de contenido de la imagen (por ejemplo, "image/jpeg" o "image/png")
+    }
 });
 
 const Pack = mongoose.model('Pack', packSchema);
